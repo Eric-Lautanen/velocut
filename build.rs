199ec6e8f70b483ca4137b60f716f058c4855cc0
Eric Lautanen <ericlautanen77@gmail.com>
2026-02-25 01:45:18 -0500

    win resource update

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
+    println!("cargo:rerun-if-changed=assets/icon.ico");
+
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("../../assets/icon.ico");
+        res.set_icon("assets/icon.ico");
         res.compile().expect("Failed to compile Windows resources");
     }
 }
